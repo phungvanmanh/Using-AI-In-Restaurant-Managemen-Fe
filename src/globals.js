4905fbca089054de10c9b01cd7a41a25c77f1b1e
trọng linh lương <tronglinhluong@gmail.com>
2024-05-08 17:46:56 +0700

    up

--- src/globals.js
+++ src/globals.js
@@ -1,7 +1,7 @@
 // export const apiUrl = "http://testbe.prodevecode.com/";
 // export const apiSocket = "http://testfe.prodevecode.com:3000";
 // export const apiQRcode = "http://testfe.prodevecode.com";
-export const apiUrl = "http://192.168.1.4:8000/";
-export const apiSocket = "http://192.168.1.4:3000";
-export const apiQRcode = "http://192.168.1.4:8001";
+export const apiUrl = "http://192.168.1.103:8000/";
+export const apiSocket = "http://192.168.1.103:3000";
+export const apiQRcode = "http://192.168.1.103:8001";
 
